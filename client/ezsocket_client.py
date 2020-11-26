4e32f71542e594f4e506a34f84c68c3bf6849b90
Jordan Zdimirovic <jordstar20001@gmail.com>
2020-11-27 00:57:59 +1100

    Main functionality is completed!

--- client/ezsocket_client.py
+++ client/ezsocket_client.py
@@ -6,45 +6,68 @@ import socket, json, threading
 
 from typing import Dict, Callable
 
+DEBUG = True
+
+#region Helpers
+def get_event_and_content(data):
+    event_end = data.find("\n")
+    event, content = data[:event_end], data[event_end + 1:].strip()
+    if content.strip() != "": content = json.loads(content)
+    else: content = None
+    return (event, content)
+#endregion
+
 class EZSClient():
     def __T_get_incoming(self):
         while self.listening:
-            data = self.TCPSOCKET.recv(self.msg_size)
             try:
-                data = data.decode()
-                event_end = data.find("\n")
-                event, content = data[:event_end], data[event_end + 1:].strip()
-                if event == "heartbeat":
-                    print("♥")
-                    continue
-                if content.strip() != "": content = json.loads(content)
-                else: content = None
-                self.__handle_event(event, content)
+                data = self.TCPSOCKET.recv(self.msg_size)
+                if data == b'':
+                    # The socket has been disconnected.
+                    self.shutdown()
+                    return
+                try:
+                    data = data.decode()
+                    event, content = get_event_and_content(data)
+                    
+                    self.__handle_event(event, content)
+
+                except Exception as e:
+                    if DEBUG: print(f"Format of data was incorrect and subsequently caused an error:\n{e}")
 
             except Exception as e:
-                print(f"Error on receiving server data:\n{e}")
+                if DEBUG: print(f"Error when receiving data:\n{e}")
+                self.shutdown()
+                return
+
 
     def __init__(self):
         self.TCPSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.event_registry = {} # event key -> callable
+        self.disconnect_callback = None
 
-    def connect(self, ip, port, msg_size = 1024):
+    def shutdown(self):
+        self.listening = False
+        if self.disconnect_callback: self.disconnect_callback()
+        # Do anything else that is necessary
+
+    def connect(self, ip, port, msg_size = 1024, disconnect_callback = None):
         self.listening = True
         self.msg_size = msg_size
         self.server_address = (ip, port)
         self.TCPSOCKET.connect(self.server_address)
+        self.disconnect_callback = disconnect_callback
         t = threading.Thread(target=self.__T_get_incoming, daemon=True)
         t.start()
 
     def on(self, event: str, callback: Callable):
-        assert event != "heartbeat"
         self.event_registry[event] = callback
     
     def __handle_event(self, event: str, data: Dict):
-        assert event in self.event_registry, "Event not found."
+        assert event in self.event_registry, f"Event {event} not found."
         self.event_registry[event](data)
 
-    def send(self, key: str, data: Dict):
+    def send(self, key: str, data: Dict = None):
         packet = key + '\n'
         packet += json.dumps(data)
         self.TCPSOCKET.send(packet.encode())
@@ -58,4 +81,6 @@ if __name__ == "__main__":
     c = EZSClient()
     c.on("test", count_keys)
     c.connect("localhost", 8080)
+    input()
+    c.send("smile", {"name": "Jordan"})
     input()
--- server/ezsocket_server.py
+++ server/ezsocket_server.py
@@ -7,6 +7,8 @@ Jordan Zdimirovic - https://github.com/jordstar20001/ez-sockets.git
 import time, sys, os, json, threading, socket, binascii, timeit
 from typing import Dict, Callable
 
+DEBUG = True
+
 #region Helper Functions
 def random_hex(length: int):
     return binascii.b2a_hex(os.urandom(length // 2)).decode()
@@ -19,24 +21,37 @@ def secs2timestr(seconds):
     seconds = seconds % 60
     number_word_pairs = [(hours, "hrs"), (minutes, "mins"), (seconds, "secs")]
     return ", ".join([f"{number} {word}" for number, word in number_word_pairs])
+def get_event_and_content(data):
+    event_end = data.find("\n")
+    event, content = data[:event_end], data[event_end + 1:].strip()
+    if content.strip() != "": content = json.loads(content)
+    else: content = None
+    return (event, content)
 #endregion
 
 class EZSConnectedClient():
     def __init__(self, server, ident, sock, address):
         self.sock = sock
-        assert type(sock) == socket.socket
+        assert type(sock) == socket.socket, "Argument 'socket' must be a valid socket."
         self.id = ident
         self.address = address
         self.server = server
-        print(type(sock))
+        self.connected = True
         self.connect_time = int(timeit.default_timer())
-        
 
     def send(self, key: str, data: Dict = None):
         packet = key + '\n'
         if data:
             packet += json.dumps(data)
-        self.sock.send(packet.encode())
+        try:
+            self.sock.send(packet.encode())
+        except ConnectionResetError:
+            self.server.disconnect_client(self.id)
+
+    def disconnect(self):
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+        self.connected = False
 
     def age(self):
         return timeit.default_timer() - self.connect_time
@@ -52,39 +67,54 @@ class EZSServer():
 
         self.__connected_clients = {}
 
-    def __T_heartbeat(self, timeout) -> None:
-        while self.listening:
-            time.sleep(timeout)
-            for _, client in self.get_clients():
-                try:
-                    client.send("heartbeat")
-                except:
-                    # Assuming that the client has disconnected.
-                    self.disconnect_client(client.id)
+        self.event_registry = {}
 
     def __T_get_incoming(self) -> None:
         while self.listening:
             sock, addr = self.TCPSOCKET.accept()
             print("Accepted.")
             token = random_hex(16)
-            self.__connected_clients[token] = EZSConnectedClient(self, token, sock, addr)
-            self.__threads[token] = threading.Thread(target=self.__T_client, args=(sock, addr))
+            client = EZSConnectedClient(self, token, sock, addr)
+            self.__connected_clients[token] = client
+            self.__threads[token] = threading.Thread(target=self.__T_client, args=(client,))
+            self.__threads[token].start()
     
+    def on(self, event, callback):
+        # TODO assert function signature
+        self.event_registry[event] = callback
+
     def get_clients(self):
         return dict2tup(self.__connected_clients)
 
     def disconnect_client(self, ident):
+        self.__connected_clients[ident].disconnect()
         del self.__connected_clients[ident]
 
-    def __T_client(self, client_sock, addr):
-        while True:
-            print(client_sock)
-            print(addr)
-            time.sleep(1)
-        pass
+    def disconnect_all(self):
+        for c, _ in self.get_clients():
+            self.disconnect_client(c)
 
-    def listen(self, port: int, callback: Callable, local: bool = False, msg_size = 1024, recv_timeout = 1, max_connections = 50, heartbeat_timeout = 3) -> None:
-        #region Bind and start listening / heartbeat threads
+    def __T_client(self, client):
+        while client.connected:
+            try:
+                data = client.sock.recv(self.msg_size)
+                data = data.decode()
+                
+                try:
+                    event, content = get_event_and_content(data)
+                except:
+                    if DEBUG:
+                        print("Data sent was in the incorrect format. Parsing to JSON failed.")
+
+                if event in self.event_registry:
+                    self.event_registry[event](client, content)
+            except:
+                if DEBUG: print(f"Client {client.id} lost connection.")
+                if client.connected:
+                    self.disconnect_client(client.id)
+
+    def listen(self, port: int, local: bool = False, msg_size = 1024, recv_timeout = 1, max_connections = 50) -> None:
+        #region Bind and start listening
         self.listening = True
 
         self.TCPSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IPV4, TCP
@@ -104,8 +134,7 @@ class EZSServer():
         #self.TCPSOCKET.connect(("localhost", self.port))
 
         self.__threads = {
-            "listening": threading.Thread(target=self.__T_get_incoming, daemon=True),
-            "heartbeat": threading.Thread(target=self.__T_heartbeat, daemon=True, args=(heartbeat_timeout,))
+            "listening": threading.Thread(target=self.__T_get_incoming, daemon=True)
         }
         
         for t in self.__threads:
@@ -122,8 +151,14 @@ class EZSServer():
 
 if __name__ == "__main__":
     s = EZSServer()
-    s.listen(8080, None, local=True)
     
+    def smile_callback(client, data):
+        print(f"☺ - Hi there, {data['name']} How are you today?")
+
+    s.on("smile", smile_callback)
+    
+    s.listen(8080, local=True)
+
     while True:
         x = input(" => ").strip().lower()
         if x.split()[0] == "send":
@@ -138,4 +173,6 @@ if __name__ == "__main__":
         elif x == "clients":
             for i, c in enumerate(s.get_clients()):
                 print(f"#{i+1}: {str(c[1])}")
+        elif x == "disconnect":
+            s.disconnect_client(s.get_clients()[0][0])
 
