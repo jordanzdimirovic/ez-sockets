76aefb0714e073000a024725b1a815b5c6a31224
Jordan Zdimirovic <jordstar20001@gmail.com>
2020-11-26 23:11:18 +1100

    Added heartbeats etc

--- client/ezsocket_client.py
+++ client/ezsocket_client.py
@@ -10,11 +10,19 @@ class EZSClient():
     def __T_get_incoming(self):
         while self.listening:
             data = self.TCPSOCKET.recv(self.msg_size)
-            print("Got data.")
-            data = data.decode()
-            event_end = data.find("\n")
-            event, data = data[:event_end], json.loads(data[event_end + 1:])
-            self.__handle_event(event, data)
+            try:
+                data = data.decode()
+                event_end = data.find("\n")
+                event, content = data[:event_end], data[event_end + 1:].strip()
+                if event == "heartbeat":
+                    print("♥")
+                    continue
+                if content.strip() != "": content = json.loads(content)
+                else: content = None
+                self.__handle_event(event, content)
+
+            except Exception as e:
+                print(f"Error on receiving server data:\n{e}")
 
     def __init__(self):
         self.TCPSOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,10 +33,11 @@ class EZSClient():
         self.msg_size = msg_size
         self.server_address = (ip, port)
         self.TCPSOCKET.connect(self.server_address)
-        t = threading.Thread(target=self.__T_get_incoming)
+        t = threading.Thread(target=self.__T_get_incoming, daemon=True)
         t.start()
 
     def on(self, event: str, callback: Callable):
+        assert event != "heartbeat"
         self.event_registry[event] = callback
     
     def __handle_event(self, event: str, data: Dict):
--- server/ezsocket_server.py
+++ server/ezsocket_server.py
@@ -4,27 +4,45 @@ EZ Socket Server for Python
 Jordan Zdimirovic - https://github.com/jordstar20001/ez-sockets.git
 """
 
-import time, sys, os, json, threading, socket, binascii
+import time, sys, os, json, threading, socket, binascii, timeit
 from typing import Dict, Callable
 
 #region Helper Functions
 def random_hex(length: int):
     return binascii.b2a_hex(os.urandom(length // 2)).decode()
+def dict2tup(d):
+    return [(k, d[k]) for k in d.keys()]
+def secs2timestr(seconds):
+    hours = seconds // 3600
+    seconds = seconds % 3600
+    minutes = seconds // 60
+    seconds = seconds % 60
+    number_word_pairs = [(hours, "hrs"), (minutes, "mins"), (seconds, "secs")]
+    return ", ".join([f"{number} {word}" for number, word in number_word_pairs])
 #endregion
 
 class EZSConnectedClient():
-    def __init__(self, server, sock, address):
+    def __init__(self, server, ident, sock, address):
         self.sock = sock
+        assert type(sock) == socket.socket
+        self.id = ident
         self.address = address
         self.server = server
+        print(type(sock))
+        self.connect_time = int(timeit.default_timer())
+        
 
-    def send(self, key: str, data: Dict):
+    def send(self, key: str, data: Dict = None):
         packet = key + '\n'
-        packet += json.dumps(data)
+        if data:
+            packet += json.dumps(data)
         self.sock.send(packet.encode())
 
-    def disconnect(self):
-        pass
+    def age(self):
+        return timeit.default_timer() - self.connect_time
+
+    def __str__(self):
+        return f"Client: {self.id}, connected {secs2timestr(int(self.age()))} ago."
 
 class EZSServer():
     def __init__(self):
@@ -34,19 +52,29 @@ class EZSServer():
 
         self.__connected_clients = {}
 
-    def __T_heartbeat(self) -> None:
-        pass
+    def __T_heartbeat(self, timeout) -> None:
+        while self.listening:
+            time.sleep(timeout)
+            for _, client in self.get_clients():
+                try:
+                    client.send("heartbeat")
+                except:
+                    # Assuming that the client has disconnected.
+                    self.disconnect_client(client.id)
 
     def __T_get_incoming(self) -> None:
         while self.listening:
             sock, addr = self.TCPSOCKET.accept()
             print("Accepted.")
             token = random_hex(16)
-            self.__connected_clients[token] = EZSConnectedClient(self, sock, addr)
+            self.__connected_clients[token] = EZSConnectedClient(self, token, sock, addr)
             self.__threads[token] = threading.Thread(target=self.__T_client, args=(sock, addr))
     
     def get_clients(self):
-        return self.__connected_clients
+        return dict2tup(self.__connected_clients)
+
+    def disconnect_client(self, ident):
+        del self.__connected_clients[ident]
 
     def __T_client(self, client_sock, addr):
         while True:
@@ -55,7 +83,7 @@ class EZSServer():
             time.sleep(1)
         pass
 
-    def listen(self, port: int, callback: Callable, local: bool = False, msg_size = 1024, recv_timeout = 1, max_connections = 50) -> None:
+    def listen(self, port: int, callback: Callable, local: bool = False, msg_size = 1024, recv_timeout = 1, max_connections = 50, heartbeat_timeout = 3) -> None:
         #region Bind and start listening / heartbeat threads
         self.listening = True
 
@@ -77,7 +105,7 @@ class EZSServer():
 
         self.__threads = {
             "listening": threading.Thread(target=self.__T_get_incoming, daemon=True),
-            "heartbeat": threading.Thread(target=self.__T_heartbeat, daemon=True)
+            "heartbeat": threading.Thread(target=self.__T_heartbeat, daemon=True, args=(heartbeat_timeout,))
         }
         
         for t in self.__threads:
@@ -99,7 +127,7 @@ if __name__ == "__main__":
     while True:
         x = input(" => ").strip().lower()
         if x.split()[0] == "send":
-            client = s.get_clients()[list(s.get_clients().keys())[0]]
+            client = s.get_clients()[0][1]
             client.send("test", {
                 "data": "yes",
                 "more": 10
@@ -107,4 +135,7 @@ if __name__ == "__main__":
         elif x == "exit":
             sys.exit()
             quit()
+        elif x == "clients":
+            for i, c in enumerate(s.get_clients()):
+                print(f"#{i+1}: {str(c[1])}")
 
